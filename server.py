f4fec5caa9765fe1eb842d97ce67659f0ce044c1
wlsdn <tjdwnsdud3@naver.com>
2021-12-16 10:59:38 +0900

    remove __main__

--- server.py
+++ server.py
@@ -119,11 +119,3 @@ async def on_shutdown(app):
     await asyncio.gather(*coros)
     pcs.clear()
 
-
-if __name__ == "__main__":
-    uvicorn.run("server:app",
-                host="0.0.0.0",
-                port=8081,
-                ssl_keyfile="./localhost+2-key.pem",
-                ssl_certfile="./localhost+2.pem",
-                reload=True)
