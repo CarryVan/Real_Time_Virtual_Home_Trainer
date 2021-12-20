43e945aa119083f81f997a00754cf426531fce33
Younghoon-Lee <8109h@naver.com>
2021-12-20 16:59:25 +0900

    hotfix requirements

--- server.py
+++ server.py
@@ -268,9 +268,4 @@ async def on_shutdown(app):
     await asyncio.gather(*coros)
     pcs.clear()
 
-if __name__ == "__main__":
-    uvicorn.run("server:app",
-                host="0.0.0.0",
-                port=8080,
-                reload=True)
 
