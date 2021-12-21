83a790c105aa98e78d1f54a3073ac10e1853214b
mingyu <aboq2496@naver.com>
2021-12-21 16:29:02 +0900

    change_model

--- VideoTransformTrack.py
+++ VideoTransformTrack.py
@@ -190,14 +190,14 @@ class VideoTransformTrack2(MediaStreamTrack):
         try:
             # self.model = models.load_model('./model/all4_model')
 
-            self.model = tf.lite.Interpreter(model_path="./model/all4_model/all4_model.tflite")
+            self.model = tf.lite.Interpreter(model_path="./model/all7_model.tflite")
             self.model.allocate_tensors()
             self.input_details = self.model.get_input_details()
             self.output_details = self.model.get_output_details()
         except Exception as e:
             print(e)
-        self.class_number = {2: 'dumbbell_u', 3: 'dumbbell_d', 6: 'legraise_u', 7: 'legraise_d', 10: 'lunge_d', 11: 'lunge_u', 13: 'plank_u', 16: 'pushup_d', 17: 'pushup_u', 
-        20: 'situp_u', 21: 'situp_d', 24: 'squat_d', 25: 'squat_u', 27: 'walking_u', 0: 'none_u'}
+        self.class_number = {8: 'legraise_u', 9: 'legraise_d', 20: 'lunge_d', 21: 'lunge_u', 27: 'plank_u', 36: 'pushup_d', 37: 'pushup_u', 
+        59: 'sitting_u', 42: 'situp_u', 43: 'situp_d', 52: 'squat_d', 53: 'squat_u', 61: 'walking_u', 0: 'none_u'}
                            
         self.detector = pm.poseDetector(
             model_dir='./model/all_model/body_language_mlp.pkl')
@@ -268,24 +268,21 @@ class VideoTransformTrack2(MediaStreamTrack):
                     self.key.remove(i)
                     
                     
-            cv2.putText(img, self.class_number[self.pose_predict], (300, 30), 
-                                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)    
-          
 
             cv2.putText(img, 'pushup' + str(self.workout_cnt['pushup']), (30, self.workout['pushup']), 
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA) 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA) 
             cv2.putText(img, 'lunge' + str(self.workout_cnt['lunge']), (30, self.workout['lunge']), 
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA) 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA) 
             cv2.putText(img, 'plank' + str(self.workout_cnt['plank']), (30, self.workout['plank']), 
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA) 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA) 
             cv2.putText(img, 'squat' + str(self.workout_cnt['squat']), (30, self.workout['squat']), 
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA) 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA) 
             cv2.putText(img, 'legraise' + str(self.workout_cnt['legraise']), (30, self.workout['legraise']), 
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA) 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA) 
             cv2.putText(img, 'situp' + str(self.workout_cnt['situp']), (30, self.workout['situp']), 
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA) 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA) 
             cv2.putText(img, 'dumbbell' + str(self.workout_cnt['dumbbell']), (30, self.workout['dumbbell']), 
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA) 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA) 
 
 
             new_frame = VideoFrame.from_ndarray(img, format="bgr24")
